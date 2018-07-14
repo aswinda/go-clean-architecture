c5b8b53cd8585eeb82c6ae7f6ec4d0a36e01ce1c
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-07-14 18:19:32 +0700

    add router event

---
+++ router.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"sync"
+
+	"github.com/go-chi/chi"
+)
+
+type IChiRouter interface {
+	InitRouter() *chi.Mux
+}
+
+type router struct{}
+
+func (router *router) InitRouter() *chi.Mux {
+
+	eventController := ServiceContainer().InjectEventController()
+
+	r := chi.NewRouter()
+	r.HandleFunc("/event/get_info", eventController.GetEventDetailAction)
+
+	return r
+}
+
+var (
+	m          *router
+	routerOnce sync.Once
+)
+
+func ChiRouter() IChiRouter {
+	if m == nil {
+		routerOnce.Do(func() {
+			m = &router{}
+		})
+	}
+	return m
+}
