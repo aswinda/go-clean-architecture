6da288d68a02f403fa236b1e62fd7bfef95981f6
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-07-17 01:51:34 +0700

    event model

---
+++ models/EventModel.go
@@ -0,0 +1,12 @@
+package models
+
+type EventModel struct {
+	id          int
+	name        string
+	description string
+	start_time  string
+	end_time    string
+	status      int
+	created_at  string
+	updated_at  string
+}
