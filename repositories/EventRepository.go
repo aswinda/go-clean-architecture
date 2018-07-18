941bad372482fa3dfefc58f36cd00980737e249a
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-07-19 02:16:26 +0700

    create event repository

--- repositories/EventRepository.go
+++ repositories/EventRepository.go
@@ -58,3 +58,54 @@ func (repository *EventRepository) EventDetail(eventId int) (models.EventModel, 
 
 	return event, nil
 }
+
+func (repository *EventRepositoryWithCircuitBreaker) StoreEvent(body models.EventModel) (models.EventModel, error) {
+	output := make(chan models.EventModel, 1)
+	hystrix.ConfigureCommand("store_event", hystrix.CommandConfig{Timeout: 1000})
+	errors := hystrix.Go("store_event", func() error {
+		event, _ := repository.EventRepository.StoreEvent(body)
+
+		output <- event
+		return nil
+	}, nil)
+
+	select {
+	case out := <-output:
+		return out, nil
+	case err := <-errors:
+		println(err)
+		return models.EventModel{}, err
+	}
+}
+
+func (repository *EventRepository) StoreEvent(body models.EventModel) (models.EventModel, error) {
+
+	queryString := fmt.Sprintf("insert into events ( name, description, start_time, end_time, status) values ('%s', '%s', '%s', '%s', '%d')", body.Name, body.Description, body.Start_time, body.End_time, 1)
+	id, err := repository.Execute(queryString)
+
+	if err != nil {
+		return models.EventModel{}, err
+	}
+
+	queryString = fmt.Sprintf("SELECT * FROM events WHERE id = '%d'", id)
+	row, err := repository.Query(queryString)
+
+	if err != nil {
+		return models.EventModel{}, err
+	}
+
+	var event models.EventModel
+
+	row.Next()
+	row.Scan(
+		&event.Id,
+		&event.Name,
+		&event.Description,
+		&event.Start_time,
+		&event.End_time,
+		&event.Status,
+		&event.Created_at,
+		&event.Updated_at)
+
+	return event, nil
+}
