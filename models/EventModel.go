f142815634c5a1872684bba9feb036cdf5496167
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-07-19 02:16:38 +0700

    add validation on event model

--- models/EventModel.go
+++ models/EventModel.go
@@ -1,12 +1,12 @@
 package models
 
 type EventModel struct {
-	Id          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Start_time  string `json:"start_time"`
-	End_time    string `json:"end_time"`
-	Status      int    `json:"status"`
+	Id          int    `json:"id" validate:"numeric"`
+	Name        string `json:"name" validate:"required,gt=2,lte=45"`
+	Description string `json:"description" validate:"required,gt=2,lte=500"`
+	Start_time  string `json:"start_time" validate:"required"`
+	End_time    string `json:"end_time" validate:"required"`
+	Status      int    `json:"status" validate:"numeric"`
 	Created_at  string `json:"created_at"`
 	Updated_at  string `json:"updated_at"`
 }
