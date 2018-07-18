673c842f2db21b796792ff9ac7bdffa8125f4daf
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-07-18 16:55:36 +0700

    validator

--- controllers/EventController.go
+++ controllers/EventController.go
@@ -1,29 +1,59 @@
 package controllers
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/aswinda/loket-backend-test/interfaces"
-	"github.com/go-chi/chi"
+	"gopkg.in/go-playground/validator.v9"
 )
+
+var validate *validator.Validate
 
 type EventController struct {
 	interfaces.IEventService
 }
 
 func (controller *EventController) GetEventDetailAction(response http.ResponseWriter, request *http.Request) {
-	eventIdParam := chi.URLParam(request, "event_id")
-	eventId, err := strconv.Atoi(eventIdParam)
+	// eventIdParam := chi.URLParam(request, "event_id")
+	// eventId, err := strconv.Atoi(eventIdParam)
 
+	queryValues := request.URL.Query()
+	eventId := queryValues.Get("event_id")
+
+	// ctx := request.Context()
+	// key := ctx.Value("event_id").(string)
+
+	// response.Write([]byte(fmt.Sprintf("hi %v", key)))
 	// validator
 
-	detail, err := controller.GetEventDetail(eventId)
+	validate = validator.New()
+	err := validate.Var(eventId, "required,numeric")
+
 	if err != nil {
-		json.NewEncoder(response).Encode("Something went wrong!!")
+		// fmt.Println(err) // output: Key: "" Error:Field validation for "" failed on the "email" tag
+		for _, err := range err.(validator.ValidationErrors) {
+
+			// fmt.Println(err.Namespace())
+			// fmt.Println(err.Field())
+			// fmt.Println(err.StructNamespace()) // can differ when a custom TagNameFunc is registered or
+			// fmt.Println(err.StructField())     // by passing alt name to ReportError like below
+			fmt.Println(err.Tag())
+			// fmt.Println(err.ActualTag())
+			// fmt.Println(err.Kind())
+			// fmt.Println(err.Type())
+			// fmt.Println(err.Value())
+			// fmt.Println(err.Param())
+			// fmt.Println()
+		}
+		return
 	}
 
-	// standarize response
-	json.NewEncoder(response).Encode(detail)
+	// detail, err := controller.GetEventDetail(eventId)
+	// if err != nil {
+	// 	json.NewEncoder(response).Encode("Something went wrong!!")
+	// }
+
+	// // standarize response
+	// json.NewEncoder(response).Encode(detail)
 }
