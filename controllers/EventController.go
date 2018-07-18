d847154a91352ab3a00e8d6cf3fea162041fb68f
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-07-19 02:15:11 +0700

    create event controller

--- controllers/EventController.go
+++ controllers/EventController.go
@@ -3,10 +3,13 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/aswinda/loket-backend-test/interfaces"
+	"github.com/aswinda/loket-backend-test/models"
 	"gopkg.in/go-playground/validator.v9"
 	"gopkg.in/matryer/respond.v1"
 )
@@ -17,7 +20,7 @@ type EventController struct {
 	interfaces.IEventService
 }
 
-func ApiResponse(code int, messages []map[string]interface{}, data map[string]interface{}) map[string]interface{} {
+func ApiResponse(code int, messages string, data map[string]interface{}) map[string]interface{} {
 	var status string
 	if code == 200 {
 		status = "ok"
@@ -44,34 +47,56 @@ func (controller *EventController) GetEventDetailAction(w http.ResponseWriter, r
 	err := validate.Var(eventId, "required,numeric")
 
 	if err != nil {
-		var errInterface map[string]interface{}
-		inrec, _ := json.Marshal(err)
-		json.Unmarshal(inrec, &errInterface)
-		respond.With(w, r, http.StatusBadRequest, ApiResponse(http.StatusBadRequest, []map[string]interface{}{errInterface}, map[string]interface{}{}))
-		// fmt.Println(err) // output: Key: "" Error:Field validation for "" failed on the "email" tag
-		for _, err := range err.(validator.ValidationErrors) {
-
-			fmt.Println(err.Tag())
-			// fmt.Println(err.ActualTag())
-			// fmt.Println(err.Kind())
-			// fmt.Println(err.Type())
-			// fmt.Println(err.Value())
-			// fmt.Println(err.Param())
-			// fmt.Println()
-		}
+		errs := err.(validator.ValidationErrors)
+		str := fmt.Sprintf("%s", errs)
+		respond.With(w, r, http.StatusBadRequest, ApiResponse(http.StatusBadRequest, str, map[string]interface{}{}))
 		return
 	}
 
 	detail, err := controller.GetEventDetail(eventId)
 	if err != nil {
-		fmt.Print("err")
+		respond.With(w, r, http.StatusBadRequest, ApiResponse(http.StatusBadRequest, fmt.Sprintf("%s", err), map[string]interface{}{}))
 	}
 
-	fmt.Println(detail)
 	var inInterface map[string]interface{}
 	inrec, _ := json.Marshal(detail)
 	json.Unmarshal(inrec, &inInterface)
-	fmt.Println(inInterface)
 
-	respond.With(w, r, http.StatusBadRequest, ApiResponse(http.StatusOK, []map[string]interface{}{}, inInterface))
+	respond.With(w, r, http.StatusBadRequest, ApiResponse(http.StatusOK, "", inInterface))
+}
+
+func (controller *EventController) CreateEventAction(w http.ResponseWriter, r *http.Request) {
+	dec := json.NewDecoder(r.Body)
+	var req models.EventModel
+	for {
+		if err := dec.Decode(&req); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	validate = validator.New()
+
+	err := validate.Struct(req)
+
+	if err != nil {
+
+		errs := err.(validator.ValidationErrors)
+		str := fmt.Sprintf("%s", errs)
+
+		respond.With(w, r, http.StatusBadRequest, ApiResponse(http.StatusBadRequest, str, map[string]interface{}{}))
+		return
+	}
+
+	result, err := controller.CreateEvent(req)
+	if err != nil {
+		respond.With(w, r, http.StatusBadRequest, ApiResponse(http.StatusBadRequest, fmt.Sprintf("%s", err), map[string]interface{}{}))
+	}
+
+	var inInterface map[string]interface{}
+	inrec, _ := json.Marshal(result)
+	json.Unmarshal(inrec, &inInterface)
+
+	respond.With(w, r, http.StatusBadRequest, ApiResponse(http.StatusOK, "", inInterface))
 }
